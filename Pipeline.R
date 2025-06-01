90002de119445b7b45f6ea86eff1bd148ee180fa
Sarjak Bhandari <bhandarisarjak@gmail.com>
2025-06-01 10:25:42 +0545

    football dataset from website

--- Pipeline.R
+++ Pipeline.R
@@ -33,4 +33,43 @@ gapminder %>%
   select(-continent,-year)%>%
   ggplot(aes(x=gdpPercap,y=lifeExp, size=pop)) + 
   geom_point(alpha=0.5) 
+#alt shift k= shortcuts
+
+
+gapminder %>% 
+  filter(year==1952) %>% 
+  summarise(num_countries=n_distinct(country), 
+            mean_pop=mean(pop))
+  
+gapminder %>% 
+  group_by(year) %>% 
+  summarise(num_countries=n_distinct(country), 
+            mean_pop=mean(pop)) 
+
+summary(gapminder)
+
+gapminder %>% 
+  group_by(year) %>% 
+  summarise(num_countries=n_distinct(country), 
+            mean_pop=mean(pop)) %>% 
+  ggplot(aes(x=year,y=mean_pop)) + 
+  geom_line()
+View(starwars)
+ncol(starwars) 
+nrow(starwars)
+
+
+starwars %>% 
+  filter(species=="Droid") %>% 
+  arrange(desc(mass))
+
+myleague = "E0"
+
+# Get data from www.football-data.co.uk
+
+url = paste0("https://www.football-data.co.uk/example.csv")  
+
+football = read_csv(url)
+
+View(football)
 
